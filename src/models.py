c7735ad6301e9448325857ca5e4a062ae847965a
Alicia Benavente <aliciabenaventegarcia@gmail.com>
2024-09-09 14:36:28 +0000

    saving nullable False

--- src/models.py
+++ src/models.py
@@ -22,22 +22,22 @@ class Post(Base):
     text = Column(String(350), nullable=False)
     likes = Column(Integer, nullable=False)
     tags = Column(Integer, nullable=False)
-    userId = Column(Integer, ForeignKey("user.id"))
+    userId = Column(Integer, ForeignKey("user.id"), nullable=False)
     user = relationship(User)
 
 class Comments(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
     text = Column(String(300), nullable=False)
-    userId = Column(Integer, ForeignKey("user.id"))
-    post_id = Column(Integer, ForeignKey('post.id'))
+    userId = Column(Integer, ForeignKey("user.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
     post = relationship(Post)
 
 class Followers(Base):
     __tablename__= "followers"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("user.id"))
-    follower_id = Column(Integer, ForeignKey("user.id"))
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    follower_id = Column(Integer, ForeignKey("user.id"), nullable=False)
 
     def to_dict(self):
         return {}
